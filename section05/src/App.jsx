81622d70133d658e5324e7891fb5764626ee5f56
Eunchae Cho <choeunchae0828@gmail.com>
2024-07-31 23:15:06 +0900

    study day 4

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,26 +1,12 @@
 import './App.css';
 import {useState} from "react";
 
-import Bulb from "./components/Bulb";
-import Counter from "./components/Counter";
-// import Header from './components/Header';
-// import Main from './components/Main';
-// import Footer from './components/Footer';
-// import Button from './components/Button';
+import Register from './components/Register';
 
 function App() {  
-  // console.log(state);
-  // const buttonProps = {
-  //   text: "메일",
-  //   color: "red",
-  //   a: 1,
-  //   b: 2,
-  //   c: 3,
-  // };
   return (
     <>
-      <Bulb />
-      <Counter />
+      <Register />
     </>
   );
 }
